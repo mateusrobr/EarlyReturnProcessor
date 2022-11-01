1902558aacb31b5ee5cdf08ac98cc1c2d710ecca
Luiz Mateus <103285835+mateusrobr@users.noreply.github.com>
2022-11-01 14:37:57 -0300

    Early return quase completp

--- TesteProcessor/src/main/java/org/example/App.java
+++ TesteProcessor/src/main/java/org/example/App.java
@@ -14,17 +14,35 @@ public class App
 {
     public static void main( String[] args )
     {
+        String path = "/home/facomp/IdeaProjects/IfCounterRepo/IfCounter/src/main/java/org/example";
+        String targetClassName = "TesteIfs";
         Launcher launcher = new Launcher();
-        launcher.addInputResource("/home/facomp/IdeaProjects/IfCounterRepo/IfCounter/src/main/java/org/example");
+        launcher.addInputResource(path);
         CtModel model = launcher.buildModel();
-        EarlyReturnProcessor earlyReturnProcessor = new EarlyReturnProcessor();
+        EarlyReturnProcessor earlyReturnProcessor = new EarlyReturnProcessor(path, targetClassName);
+        CtClass targetClassSpoon = null;
 
 
-        //System.out.println(model);
-        CtMethod testeMetodo = model.getElements(new TypeFilter<>(CtClass.class)).get(3).getElements(new TypeFilter<>(CtMethod.class)).get(0);
+        for (int i = 0 ; i < model.getElements(new TypeFilter<>(CtClass.class)).size() ; i++){
+            if (model.getElements(new TypeFilter<>(CtClass.class)).get(i).getSimpleName().equals(targetClassName)){
+                targetClassSpoon = model.getElements(new TypeFilter<>(CtClass.class)).get(i);
+            }
+        }
+        for (CtMethod method : targetClassSpoon.getElements(new TypeFilter<>(CtMethod.class))){
+            System.out.println(method);
+
+            earlyReturnProcessor.process(method);
+            System.out.println(method);
+        }
+
+
+        /*CtMethod testeMetodo = model.getElements(new TypeFilter<>(CtClass.class)).get(3).getElements(new TypeFilter<>(CtMethod.class)).get(0);
         System.out.println(testeMetodo.prettyprint());
+        earlyReturnProcessor.earlyReturnRefactor();
+        //CtMethod testeMetodo = model.getElements(new TypeFilter<>(CtClass.class)).get(3).getElements(new TypeFilter<>(CtMethod.class)).get(0);
 
-        earlyReturnProcessor.process(testeMetodo);
+        //earlyReturnProcessor.process(testeMetodo);
         System.out.println(testeMetodo.prettyprint());
+        //earlyReturnProcessor.process(testeMetodo);*/
     }
 }
--- TesteProcessor/src/main/java/org/example/EarlyReturnProcessor.java
+++ TesteProcessor/src/main/java/org/example/EarlyReturnProcessor.java
@@ -1,18 +1,69 @@
 package org.example;
 
 import spoon.Launcher;
+import spoon.reflect.CtModel;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtIf;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtStatement;
-import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 public class EarlyReturnProcessor extends AbstractProcessor<CtMethod>{
+    private String path;
+    private String targetClassName;
+
+    public EarlyReturnProcessor(String path, String targetClassName){
+        this.path = path;
+        this.targetClassName = targetClassName;
+    }
+
+    public void earlyReturnRefactor() {
+        Launcher launcher = new Launcher();
+        launcher.addInputResource(path);
+        CtModel model = launcher.buildModel();
+        CtClass targetClassSpoon = null;
+        for (int i = 0; i < model.getElements(new TypeFilter<>(CtClass.class)).size(); i++) {
+            if (model.getElements(new TypeFilter<>(CtClass.class)).get(i).getSimpleName().equals(targetClassName)) {
+                targetClassSpoon = model.getElements(new TypeFilter<>(CtClass.class)).get(i);
+            }
+        }
+        System.out.println("A");
+
+        if(targetClassSpoon.getSimpleName() == null){
+            return;
+        }
+        System.out.println("B");
+
+        for(int i = 0 ; i < targetClassSpoon.getElements(new TypeFilter<>(CtMethod.class)).size() ; i++){
+            System.out.println(targetClassSpoon.getElements(new TypeFilter<>(CtMethod.class)).get(i));
+            process(targetClassSpoon.getElements(new TypeFilter<>(CtMethod.class)).get(i));
+        }
+        System.out.println("C");
+
+
+    }
+
 
     @Override
     public void process(CtMethod ctMethod) {
+        if(!ctMethod.getType().prettyprint().equals("void")){
+            return;
+        }
+
+        if(ctMethod.getElements(new TypeFilter<>(CtIf.class)).size() == 0 || ctMethod.getElements(new TypeFilter<>(CtIf.class)).size() > 1){
+            return;
+        }
+
+        if(ctMethod.getElements(new TypeFilter<>(CtIf.class)).get(0).getElseStatement() == null){
+            return;
+        }
+
+        if(ctMethod.getElements(new TypeFilter<>(CtIf.class)).get(0).getElseStatement().getElements(new TypeFilter<>(CtIf.class)).size() > 0 ){
+            return;
+        }
+
         Launcher launcher = new Launcher();
         CtStatement returnStatement = launcher.getFactory().createCodeSnippetStatement("return");
 
