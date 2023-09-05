92e9a5afea0fd0d1f190abb5c8b6daa54e0db54f
Luiz Mateus <103285835+mateusrobr@users.noreply.github.com>
2023-09-05 17:43:55 -0300

    no starting from command line

--- TesteProcessor/src/main/java/org/example/App.java
+++ TesteProcessor/src/main/java/org/example/App.java
@@ -20,34 +20,36 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
-        if(args.length == 2){
-            String pathHome = args[0];
-            String targetMethod = args[1];
-            MoveMethodRefefactoring moveMethod = new MoveMethodRefefactoring(pathHome, targetMethod);
-            for(Map.Entry<PDGSlice, CtMethod>entry : moveMethod.getCandidateMap().entrySet()){
-                entry.getKey().printSlice();
-                System.out.println(entry.getValue().prettyprint());
-                System.out.println("If statements that need to continue in the original Method");
-                System.out.println(moveMethod.getPdg().getRemaingNodes().get(entry.getKey().getLocalVariable()));
-                //System.out.println(entry.getKey().getMapAux());
-            }
-        }
-        else{
-            System.out.println("Incorrect Number of arguments, the correct way to use it is first argument being the path to the class and the seconde one being the name of the method ");
-        }
+//        if(args.length == 2){
+//            String pathHome = args[0];
+//            String targetMethod = args[1];
+//            MoveMethodRefefactoring moveMethod = new MoveMethodRefefactoring(pathHome, targetMethod);
+//            for(Map.Entry<PDGSlice, CtMethod>entry : moveMethod.getCandidateMap().entrySet()){
+//                entry.getKey().printSlice();
+//                System.out.println(entry.getValue().prettyprint());
+//                System.out.println("If statements that need to continue in the original Method");
+//                System.out.println(moveMethod.getPdg().getRemaingNodes().get(entry.getKey().getLocalVariable()));
+//                //System.out.println(entry.getKey().getMapAux());
+//            }
+//        }
+//        else{
+//            System.out.println(args.length);
+//            System.out.println("Incorrect Number of arguments, the correct way to use it is first argument being the path to the class and the seconde one being the name of the method ");
+//        }
 
-//        String path = "/home/facomp/IdeaProjects/IfCounterRepo/IfCounter/src/main/java/org/example";
-//        //String path = "C:\\Users\\Kurumi\\Desktop\\ERP\\EarlyReturnProcessor\\TesteProcessor\\src\\main\\java\\BaseMethodsForTesting";
-//        String pathExample = "C:\\Users\\Kurumi\\Desktop\\SpoonTests\\spoonTests\\src\\main\\java\\org\\example";
-//        String pathHome = "C:\\Users\\Mateus\\Desktop\\metodosteste\\testemetodos\\src";
-//        String targetClassName = "Main";
-//        String targetMethod = "printDocument";
-//        Launcher launcher = new Launcher();
-//        launcher.addInputResource(pathHome);
-//        launcher.buildModel();
-//        CtMethod method = (CtMethod) launcher.getModel().getElements(new NamedElementFilter(CtMethod.class, "printDocument")).get(0);
-//        //System.out.println(method.prettyprint());
-//        PDG pdg = new PDG(method);
+        String path = "/home/facomp/IdeaProjects/IfCounterRepo/IfCounter/src/main/java/org/example";
+        //String path = "C:\\Users\\Kurumi\\Desktop\\ERP\\EarlyReturnProcessor\\TesteProcessor\\src\\main\\java\\BaseMethodsForTesting";
+        String pathExample = "C:\\Users\\Kurumi\\Desktop\\SpoonTests\\spoonTests\\src\\main\\java\\org\\example";
+        String pathHome = "C:\\Users\\Mateus\\Desktop\\metodosteste\\testemetodos\\src";
+        String pathDell = "/home/dev/Área de Trabalho/códigos-teste/untitled";
+        String targetClassName = "Main";
+        String targetMethod = "printDocument";
+        Launcher launcher = new Launcher();
+        launcher.addInputResource(pathDell);
+        launcher.buildModel();
+        CtMethod method = (CtMethod) launcher.getModel().getElements(new NamedElementFilter(CtMethod.class, "printDocument")).get(0);
+        //System.out.println(method.prettyprint());
+        PDG pdg = new PDG(method);
 //        for(GraphNode node : pdg.getCfg().getAllNodes()){
 //            System.out.println(node);
 //            for(GraphEdgeNode edge : node.getOutgoingEdges()){
--- TesteProcessor/src/test/java/org/example/EarlyReturnProcessorTest.java
+++ TesteProcessor/src/test/java/org/example/EarlyReturnProcessorTest.java
@@ -11,32 +11,32 @@ import static org.junit.Assert.*;
 
 public class EarlyReturnProcessorTest {
 
-    @Test
-    public void EarlyReturnProcess() {
-        EarlyReturnProcessor earlyReturnProcessor = new EarlyReturnProcessor();
-
-        CtClass targetClassSpoon = getTargetClass();
-
-        earlyReturnProcessor.process((CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(0));
-
-        CtMethod expectedMethod = (CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(1);
-        CtMethod actualMethod = (CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(0);
-
-
-        assertEquals(expectedMethod.getBody().prettyprint(), actualMethod.getBody().prettyprint());
-
-    }
-
-    public CtClass getTargetClass(){
-        Launcher launcher = new Launcher();
-        launcher.addInputResource("F:\\Bolsa\\EarlyReturnProcessor\\TesteProcessor\\src\\main\\java\\BaseMethodsForTesting");
-        launcher.buildModel();
-        CtModel model = launcher.getModel();
-
-        CtClass targetClassSpoon = model.getElements(new TypeFilter<>(CtClass.class)).get(0);
-        System.out.println(targetClassSpoon);
-
-        return targetClassSpoon;
-    }
+//    @Test
+//    public void EarlyReturnProcess() {
+//        EarlyReturnProcessor earlyReturnProcessor = new EarlyReturnProcessor();
+//
+//        CtClass targetClassSpoon = getTargetClass();
+//
+//        earlyReturnProcessor.process((CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(0));
+//
+//        CtMethod expectedMethod = (CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(1);
+//        CtMethod actualMethod = (CtMethod) targetClassSpoon.getElements(new TypeFilter(CtMethod.class)).get(0);
+//
+//
+//        assertEquals(expectedMethod.getBody().prettyprint(), actualMethod.getBody().prettyprint());
+//
+//    }
+//
+//    public CtClass getTargetClass(){
+//        Launcher launcher = new Launcher();
+//        launcher.addInputResource("F:\\Bolsa\\EarlyReturnProcessor\\TesteProcessor\\src\\main\\java\\BaseMethodsForTesting");
+//        launcher.buildModel();
+//        CtModel model = launcher.getModel();
+//
+//        CtClass targetClassSpoon = model.getElements(new TypeFilter<>(CtClass.class)).get(0);
+//        System.out.println(targetClassSpoon);
+//
+//        return targetClassSpoon;
+//    }
 
 }
